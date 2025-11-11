c3eaa91fd9630f09331047d467e624ba78ccaae1
Baris Gorgun <BARISGO1@intertech.com.tr>
2025-11-11 15:08:45 +0300

    added new app with swiftUI

---
+++ PokeVerseSwiftUI/PokeVerseSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeVerseSwiftUIApp.swift
+//  PokeVerseSwiftUI
+//
+//  Created by Gorgun, Baris on 11.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct PokeVerseSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
